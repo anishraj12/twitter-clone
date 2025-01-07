c536eb204d16af5be8cad4dc8a9c933e637f893a
Anish Raj <itsanishhere12@gmail.com>
2025-01-07 19:01:03 +0530

    final Backend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,41 @@
+import path from "path";
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { v2 as cloudinary } from "cloudinary";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => {
